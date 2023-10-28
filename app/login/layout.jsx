0ae9a54ea785c693bb56e4a72e6fd87b582242ec
amieow <109803246+amieow@users.noreply.github.com>
2023-10-29 00:02:29 +0700

    fix-bug #2

--- app/login/layout.jsx
+++ app/login/layout.jsx
@@ -1,6 +1,6 @@
-import LoginLayout from "@/components/organisme/layout/loginLayout";
+import LoginLayout from "@/components/organisme/layout/LoginLayout";
 import React from "react";
 
 export default function Layout({ children }) {
-  return <LoginLayout>{children}</LoginLayout>;
+	return <LoginLayout>{children}</LoginLayout>;
 }
